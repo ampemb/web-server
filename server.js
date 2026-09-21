4597d60e9f5d9bf7dc7cdfd38bd21f7aa20d129c
Agustin Pemberton <apemberton@umass.edu>
2026-09-21 20:07:51 +0000

    add About page

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 const app = express();
+app.set("view engine", "ejs");
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
@@ -8,35 +9,35 @@ app.get("/", (req, res) => {
 });
 
 app.get("/about", (req, res) => {
-  res.send("This is a web programming course");
+  res.render("about", { title: "About" });
 });
 
 // Unit 2 Exercise
 
-app.get("/hello/:name", (req, res) => {
-  const name = req.params.name;
-  res.send(`Hello, ${name}!`);
-});
+// app.get("/hello/:name", (req, res) => {
+//   const name = req.params.name;
+//   res.send(`Hello, ${name}!`);
+// });
 
-app.get("/repeat/:word", (req, res) => {
-  const word = req.params.word;
-  res.send(`${word} ${word} ${word}`);
-});
+// app.get("/repeat/:word", (req, res) => {
+//   const word = req.params.word;
+//   res.send(`${word} ${word} ${word}`);
+// });
 
-app.get("/count", (req, res) => {
-  const from = req.query.from || "1";
-  const to = req.query.to || "10";
-  res.send(`Counting from "${from}", to ${to} results.`);
-});
+// app.get("/count", (req, res) => {
+//   const from = req.query.from || "1";
+//   const to = req.query.to || "10";
+//   res.send(`Counting from "${from}", to ${to} results.`);
+// });
 
-app.get("/api/info", (req, res) => {
-  const userInfo = { name: "Jane", id: 123 };
-  res.json(userInfo);
-});
+// app.get("/api/info", (req, res) => {
+//   const userInfo = { name: "Jane", id: 123 };
+//   res.json(userInfo);
+// });
 
-app.get("/api/error", (req, res) => {
-  res.status(400).send("Bad Request.");
-});
+// app.get("/api/error", (req, res) => {
+//   res.status(400).send("Bad Request.");
+// });
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
